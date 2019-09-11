3305266812910c44c0d214020dabcefabd4f7a2b
harish <harish.rana@staqu.com>
2019-09-11 08:59:41 +0530

    Add new vlidation and sanitaization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser:true,
@@ -9,9 +10,40 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
   name:{
     type: String,
+    required:true,
+    trim:true,
+  },
+  email:{
+    type:String,
+    required:true,
+    trim:true,
+    lowercase:true,
+    validate(value) {
+      if(!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    }
+  },
+  password:{
+    type:String,
+    required:true,
+    trim:true,
+    validate(value) {
+      if(value.length < 6) {
+        throw new Error("length of string should be greater than 6");
+      } else if(value.includes("password")) {
+        throw new Error("It must not contain password");
+      }
+    }
   }, 
   age:{
-    type:Number
+    type:Number,
+    default:0,
+    validate(value) {
+      if(value < 0) {
+        throw new Error("Age must be a poistive number");
+      }
+    }
   }
 });
 
