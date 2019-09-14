eb427069414c17a60633e345955a5814ed920db8
harish <harish.rana@staqu.com>
2019-09-14 17:44:37 +0530

    Try out async await with mongoose

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,10 +1,23 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5d7c6de5fd2bcd1210f2ed92')
-.then(task => {
-  console.log(task);
-  return Task.countDocuments({completed:true})
-}).then(count => {
-  console.log(count);
+// Task.findByIdAndDelete('5d7c6de5fd2bcd1210f2ed92')
+// .then(task => {
+//   console.log(task);
+//   return Task.countDocuments({completed:true})
+// }).then(count => {
+//   console.log(count);
+// })
+
+const findByIdAndDeleteAsync = async (id) => {
+  const deletedTask = await Task.findByIdAndDelete(id);
+  console.log(deletedTask);
+  const count = await Task.countDocuments({completed:false});
+  return count;
+}
+
+findByIdAndDeleteAsync("5d7c747053bcd3157af3a806").then(res => {
+  console.log(res);
+}).catch(error => {
+  console.log(error);
 })
