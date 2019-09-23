e159d585ce0fcd47bb5e55b681ec11f37c8ccf01
harish <harish.rana@staqu.com>
2019-09-23 09:02:23 +0530

    Chnage Task APIs to let authenticate user to create task

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Task = require('../models/task');
- 
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 
@@ -83,9 +83,13 @@ router.get('/tasks', async (req,res) => {
   //  })
 })
 
-router.post('/tasks', async (req,res) => {
-  const task = new Task(req.body);
+router.post('/tasks', auth ,async (req,res) => {
+  //const task = new Task(req.body);
 
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id
+  })
   try {
     await task.save();
     res.status(200).send(task);  
