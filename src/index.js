49a179bac77a523c2fa6682c1fb7237e5c030fbb
Harish Rana <harishrana@Harishs-MacBook-Pro.local>
2019-10-31 17:28:28 +0530

    Add api to fetch user profile picture

--- src/index.js
+++ src/index.js
@@ -6,33 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3001;
 
-const multer = require('multer');
-const upload = multer({
-  dest:'uploads',
-  limits:{
-    fileSize:1000000
-  },
-  fileFilter(req,file,cb) {
-    // console.log(req,file);
-    // if(!file.originalname.endsWith('.pdf')) {
-    if(!file.originalname.match(/\.(doc|docx)$/)) {
-      return cb(new Error('please upload a word document'));
-    }
-    cb(undefined, true)
-  }
-})
-// app.use((req, res, next) => {
-//   res.status(503).send("site is currently down");
-  // next();
-// })
-
-app.post('/upload', upload.single('upload') ,async (req, res) => {
-  res.send();
-},(error,req,res,next) => {
-  res.status(400).send({ error: error.message})
-})
-
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
